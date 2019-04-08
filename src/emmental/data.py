48f742bf80b22097116adfaad98fae1245a214c0
Sen Wu <senwu@cs.stanford.edu>
2019-04-07 23:21:23 -0700

    add add/remove feature/label funcs in dataset

--- src/emmental/data.py
+++ src/emmental/data.py
@@ -3,15 +3,17 @@ from collections import defaultdict
 from emmental.utils.utils import list_to_tensor
 from torch.utils.data import DataLoader, Dataset
 
+# TODO: add different batch size support for different label
+
 
 class EmmentalDataset(Dataset):
     """A advanced dataset class to handle that the input data contains mulitple
     fields and the output data contains multiple label sets
 
-    :param X_dict: the input dict where key is the field name and value is the
-    data
+    :param X_dict: the feature dict where key is the feature name and value is the
+    feature
     :type X_dict: dict
-    :param Y_dict: the output dict where key is the label set name and value is
+    :param Y_dict: the label dict where key is the label name and value is
     the label
     :type Y_dict: dict
     """
@@ -21,12 +23,56 @@ class EmmentalDataset(Dataset):
         self.Y_dict = Y_dict
 
     def __getitem__(self, index):
-        x_dict = {key: field[index] for key, field in self.X_dict.items()}
-        y_dict = {key: label[index] for key, label in self.Y_dict.items()}
+        x_dict = {name: feature[index] for name, feature in self.X_dict.items()}
+        y_dict = {name: label[index] for name, label in self.Y_dict.items()}
         return x_dict, y_dict
 
     def __len__(self):
         return len(next(iter(self.X_dict.values())))
+
+    def _update_dict(self, ori_dict, new_dict):
+        for key, value in new_dict.items():
+            ori_dict[key] = value
+
+    def _remove_key(self, ori_dict, key):
+        if key in ori_dict:
+            del ori_dict[key]
+
+    def add_features(self, X_dict):
+        """Add new features into X_dict
+
+        :param X_dict: the new feature dict to add into the existing feature dict
+        :type X_dict: dict
+        """
+
+        self._update_dict(self.X_dict, X_dict)
+
+    def add_labels(self, Y_dict):
+        """Add new labels into Y_dict
+
+        :param Y_dict: the new label dict to add into the existing label dict
+        :type Y_dict: dict
+        """
+
+        self._update_dict(self.Y_dict, Y_dict)
+
+    def remove_feature(self, feature_name):
+        """Remove one feature from feature dict
+
+        :param feature_name: the feature that removes from feature dict
+        :type feature_name: str
+        """
+
+        self._remove_key(self.X_dict, feature_name)
+
+    def remove_label(self, label_name):
+        """Remove one label from label dict
+
+        :param label_name: the label that removes from label dict
+        :type label_name: str
+        """
+
+        self._remove_key(self.Y_dict, label_name)
 
 
 def emmental_collate_fn(batch):
@@ -46,7 +92,7 @@ def emmental_collate_fn(batch):
     for label_name, values in Y_batch.items():
         Y_batch[label_name] = list_to_tensor(values)
 
-    return X_batch, Y_batch
+    return dict(X_batch), dict(Y_batch)
 
 
 class EmmentalDataLoader(DataLoader):
---
+++ tests/data/test_data.py
@@ -0,0 +1,155 @@
+#! /usr/bin/env python
+import logging
+
+import torch
+from emmental.data import EmmentalDataLoader, EmmentalDataset
+
+
+def test_emmental_dataset(caplog):
+    """Unit test of emmental dataset"""
+
+    caplog.set_level(logging.INFO)
+
+    x1 = [
+        torch.Tensor([1]),
+        torch.Tensor([1, 2]),
+        torch.Tensor([1, 2, 3]),
+        torch.Tensor([1, 2, 3, 4]),
+        torch.Tensor([1, 2, 3, 4, 5]),
+    ]
+
+    y1 = [
+        torch.Tensor([0]),
+        torch.Tensor([0]),
+        torch.Tensor([0]),
+        torch.Tensor([0]),
+        torch.Tensor([0]),
+    ]
+
+    dataset = EmmentalDataset({"data1": x1}, {"label1": y1})
+
+    # Check if the dataset is correctly constructed
+    assert torch.equal(dataset[0][0]["data1"], torch.Tensor([1]))
+    assert torch.equal(dataset[0][1]["label1"], torch.Tensor([0]))
+
+    x2 = [
+        torch.Tensor([1, 2, 3, 4, 5]),
+        torch.Tensor([1, 2, 3, 4]),
+        torch.Tensor([1, 2, 3]),
+        torch.Tensor([1, 2]),
+        torch.Tensor([1]),
+    ]
+
+    dataset.add_features({"data2": x2})
+
+    # Check add one more feature to dataset
+    assert torch.equal(dataset[0][0]["data2"], torch.Tensor([1, 2, 3, 4, 5]))
+
+    y2 = [
+        torch.Tensor([1]),
+        torch.Tensor([1]),
+        torch.Tensor([1]),
+        torch.Tensor([1]),
+        torch.Tensor([1]),
+    ]
+
+    dataset.add_labels({"label2": y2})
+
+    # Check add one more label to dataset
+    assert torch.equal(dataset[0][1]["label2"], torch.Tensor([1]))
+
+    dataset.remove_label("label1")
+
+    # Check remove one more label to dataset
+    assert "label1" not in dataset.Y_dict
+
+
+def test_emmental_dataloader(caplog):
+    """Unit test of emmental dataloader"""
+
+    caplog.set_level(logging.INFO)
+
+    x1 = [
+        torch.Tensor([1]),
+        torch.Tensor([1, 2]),
+        torch.Tensor([1, 2, 3]),
+        torch.Tensor([1, 2, 3, 4]),
+        torch.Tensor([1, 2, 3, 4, 5]),
+    ]
+
+    y1 = [
+        torch.Tensor([0]),
+        torch.Tensor([0]),
+        torch.Tensor([0]),
+        torch.Tensor([0]),
+        torch.Tensor([0]),
+    ]
+
+    x2 = [
+        torch.Tensor([1, 2, 3, 4, 5]),
+        torch.Tensor([1, 2, 3, 4]),
+        torch.Tensor([1, 2, 3]),
+        torch.Tensor([1, 2]),
+        torch.Tensor([1]),
+    ]
+
+    y2 = [
+        torch.Tensor([1]),
+        torch.Tensor([1]),
+        torch.Tensor([1]),
+        torch.Tensor([1]),
+        torch.Tensor([1]),
+    ]
+
+    dataset = EmmentalDataset({"data1": x1, "data2": x2}, {"label1": y1, "label2": y2})
+
+    dataloader1 = EmmentalDataLoader(dataset, batch_size=2)
+
+    x_batch, y_batch = next(iter(dataloader1))
+
+    # Check if the dataloader is correctly constructed
+    assert torch.equal(x_batch["data1"], torch.Tensor([[1, 0], [1, 2]]))
+    assert torch.equal(
+        x_batch["data2"], torch.Tensor([[1, 2, 3, 4, 5], [1, 2, 3, 4, 0]])
+    )
+    assert torch.equal(y_batch["label1"], torch.Tensor([[0], [0]]))
+    assert torch.equal(y_batch["label2"], torch.Tensor([[1], [1]]))
+
+    dataloader2 = EmmentalDataLoader(dataset, batch_size=3)
+
+    x_batch, y_batch = next(iter(dataloader2))
+
+    # Check if the dataloader with differet batch size is correctly constructed
+    assert torch.equal(
+        x_batch["data1"], torch.Tensor([[1, 0, 0], [1, 2, 0], [1, 2, 3]])
+    )
+    assert torch.equal(
+        x_batch["data2"],
+        torch.Tensor([[1, 2, 3, 4, 5], [1, 2, 3, 4, 0], [1, 2, 3, 0, 0]]),
+    )
+    assert torch.equal(y_batch["label1"], torch.Tensor([[0], [0], [0]]))
+    assert torch.equal(y_batch["label2"], torch.Tensor([[1], [1], [1]]))
+
+    y3 = [
+        torch.Tensor([2]),
+        torch.Tensor([2]),
+        torch.Tensor([2]),
+        torch.Tensor([2]),
+        torch.Tensor([2]),
+    ]
+
+    dataset.Y_dict["label2"] = y3
+
+    x_batch, y_batch = next(iter(dataloader1))
+    # Check dataloader is correctly updated with update dataset
+    assert torch.equal(
+        x_batch["data2"], torch.Tensor([[1, 2, 3, 4, 5], [1, 2, 3, 4, 0]])
+    )
+    assert torch.equal(y_batch["label2"], torch.Tensor([[2], [2]]))
+
+    x_batch, y_batch = next(iter(dataloader2))
+    assert torch.equal(
+        x_batch["data2"],
+        torch.Tensor([[1, 2, 3, 4, 5], [1, 2, 3, 4, 0], [1, 2, 3, 0, 0]]),
+    )
+    assert torch.equal(y_batch["label2"], torch.Tensor([[2], [2], [2]]))
