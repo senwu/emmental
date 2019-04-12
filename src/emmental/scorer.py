f8a9f60f839b3514611da5f465acef2a43b2c3a1
Sen Wu <senwu@cs.stanford.edu>
2019-04-12 14:11:01 -0700

    add scorer and test

---
+++ src/emmental/scorer.py
@@ -0,0 +1,38 @@
+import logging
+
+from emmental.metrics import METRICS
+
+
+class Scorer(object):
+    """A class to score tasks
+
+    :param metrics: a list of metric names which provides in emmental (e.g., accuracy)
+    :type metrics: list
+    :param customize_metric_funcs: a dict of customize metric where key is the metric
+    name and value is the metric function which takes gold, preds, probs as input
+    :type customize_metric_funcs: dict
+    """
+
+    def __init__(self, metrics=[], customize_metric_funcs={}):
+        self.logger = logging.getLogger(__name__)
+
+        self.metrics = dict()
+        for metric in metrics:
+            if metric not in METRICS:
+                raise ValueError(f"Unrecognized metric: {metric}")
+            self.metrics[metric] = METRICS[metric]
+
+        self.metrics.update(customize_metric_funcs)
+
+    def score(self, gold, preds, probs):
+        metric_dict = dict()
+
+        for metric_name, metric in self.metrics.items():
+            res = metric(gold, preds, probs)
+
+            if isinstance(res, dict):
+                metric_dict.update(res)
+            else:
+                metric_dict[metric_name] = res
+
+        return metric_dict
---
+++ tests/test_scorer.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+import logging
+
+import numpy as np
+
+from emmental.scorer import Scorer
+
+
+def test_scorer(caplog):
+    """Unit test of scorer"""
+
+    caplog.set_level(logging.INFO)
+
+    golds = np.array([1, 0, 1, 0, 1, 0])
+    preds = np.array([1, 1, 1, 1, 1, 0])
+    probs = np.array([0.8, 0.6, 0.9, 0.7, 0.7, 0.2])
+
+    def sum(gold, preds, probs):
+        return np.sum(preds)
+
+    scorer = Scorer(metrics=["accuracy", "f1"], customize_metric_funcs={"sum": sum})
+
+    assert scorer.score(golds, preds, probs) == {
+        "accuracy": 0.6666666666666666,
+        "f1": 0.7499999999999999,
+        "sum": 5,
+    }
