1eda9fc18950304e14e3e05222d83cffc1491272
Sen Wu <senwu@cs.stanford.edu>
2019-04-13 01:42:22 -0700

    clean task

--- src/emmental/task.py
+++ src/emmental/task.py
@@ -19,33 +19,3 @@ class Task(ABC):
     def __repr__(self):
         cls_name = type(self).__name__
         return f"{cls_name}(name={self.name})"
-
-
-# class MultiTask(ABC):
-#     """A abstract calss for multi-task in Emmental MTL model.
-#     """
-
-#     def __init__(self, tasks, name=None):
-#         self.name = name if name is not None else type(self).__name__
-#         self.modules = nn.ModuleDict()
-#         self.task_flows = dict()
-#         self.loss_funcs = dict()
-#         self.output_funcs = dict()
-
-#         for task in tasks:
-#             # Combine modules from all tasks
-#             for key in task.modules.keys():
-#                 if key in self.modules:
-#                     task.modules[key] = self.modules[key]
-#                 else:
-#                     self.modules[key] = task.modules[key]
-#             # Collect task flows
-#             self.task_flows[task.name] = task.task_flow
-#             # Collect loss functions
-#             self.loss_funcs[task.name] = task.loss_func
-#             # Collect output functions
-#             self.output_funcs[task.name] = task.output_func
-
-#     def __repr__(self):
-#         cls_name = type(self).__name__
-#         return f"{cls_name}(name={self.name})"
