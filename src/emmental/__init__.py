0e7391674905d2058a38a6f90d5eb5193fab394a
Sen Wu <senwu@cs.stanford.edu>
2019-04-18 15:59:13 -0700

    update init

--- src/emmental/__init__.py
+++ src/emmental/__init__.py
@@ -1,5 +1,4 @@
 from emmental._version import __version__
 from emmental.meta import Meta, init_logging
-from emmental.task import Task
 
-__all__ = ["__version__", "Meta", "Task", "init_logging"]
+__all__ = ["__version__", "Meta", "init_logging"]
