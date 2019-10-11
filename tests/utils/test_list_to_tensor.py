b844c6d33cf69f807abea430d7b0cfd6aa08072f
Sen Wu <senwu@cs.stanford.edu>
2019-10-10 17:22:09 -0700

    fix  test

--- tests/utils/test_list_to_tensor.py
+++ tests/utils/test_list_to_tensor.py
@@ -13,16 +13,18 @@ def test_list_to_tensor(caplog):
     # list of 1-D tensor with the different length
     batch = [torch.Tensor([1, 2]), torch.Tensor([3]), torch.Tensor([4, 5, 6])]
 
-    padded_batch = list_to_tensor(batch)
+    padded_batch, mask_batch = list_to_tensor(batch)
 
     assert torch.equal(padded_batch, torch.Tensor([[1, 2, 0], [3, 0, 0], [4, 5, 6]]))
+    assert torch.equal(mask_batch, torch.BoolTensor([[0, 0, 1], [0, 1, 1], [0, 0, 0]]))
 
     # list of 1-D tensor with the same length
     batch = [torch.Tensor([1, 2, 3]), torch.Tensor([4, 5, 6]), torch.Tensor([7, 8, 9])]
 
-    padded_batch = list_to_tensor(batch)
+    padded_batch, mask_batch = list_to_tensor(batch)
 
     assert torch.equal(padded_batch, torch.Tensor([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+    assert torch.equal(mask_batch, torch.BoolTensor([[0, 0, 0], [0, 0, 0], [0, 0, 0]]))
 
     # list of 2-D tensor with the same size
     batch = [
@@ -31,7 +33,7 @@ def test_list_to_tensor(caplog):
         torch.Tensor([[7, 8, 9], [7, 8, 9]]),
     ]
 
-    padded_batch = list_to_tensor(batch)
+    padded_batch, mask_batch = list_to_tensor(batch)
 
     assert torch.equal(
         padded_batch,
@@ -39,6 +41,7 @@ def test_list_to_tensor(caplog):
             [[[1, 2, 3], [1, 2, 3]], [[4, 5, 6], [4, 5, 6]], [[7, 8, 9], [7, 8, 9]]]
         ),
     )
+    assert mask_batch is None
 
     # list of tensor with the different size
     batch = [
@@ -47,8 +50,11 @@ def test_list_to_tensor(caplog):
         torch.Tensor([7, 8, 9, 0]),
     ]
 
-    padded_batch = list_to_tensor(batch)
+    padded_batch, mask_batch = list_to_tensor(batch)
 
     assert torch.equal(
         padded_batch, torch.Tensor([[1, 2, 2, 3], [4, 5, 6, 0], [7, 8, 9, 0]])
+    )
+    assert torch.equal(
+        mask_batch, torch.BoolTensor([[0, 0, 0, 0], [0, 0, 0, 1], [0, 0, 0, 1]])
     )
