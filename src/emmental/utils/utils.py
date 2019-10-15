5f5d2346c3f422bbb7a0c67e1780a5d661021818
Sen Wu <senwu@cs.stanford.edu>
2019-10-14 17:57:15 -0700

    add type for utils

--- src/emmental/utils/utils.py
+++ src/emmental/utils/utils.py
@@ -1,11 +1,14 @@
 import random
 import string
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import numpy as np
 import torch
+from numpy import ndarray
+from torch import Tensor
 
 
-def set_random_seed(seed):
+def set_random_seed(seed: int) -> None:
     """Set random seed."""
 
     seed = int(seed)
@@ -17,13 +20,22 @@ def set_random_seed(seed):
 
     # Set random seed for PyTorch
     torch.manual_seed(seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)
 
 
-def list_to_tensor(item_list, min_len=0, max_len=0):
-    """Convert the list of torch.Tensor into a torch.Tensor."""
+def list_to_tensor(
+    item_list: List[Tensor], min_len: int = 0, max_len: int = 0
+) -> Union[Tuple[Tensor, None], Tuple[Tensor, Tensor]]:
+    """Convert the list of torch.Tensor into a torch.Tensor.
+
+    :param item_list: The data for converting.
+    :type batch: list of torch.Tensor
+    :param min_len: Min length of sequence of data.
+    :type min_len: int
+    :param max_len: Max length of sequence of data.
+    :type max_len: int
+    :return: The converted tensor and the correspoing mask tensor.
+    :rtype: tuple of torch.Tensors with shape (batch_size, max_seq_len) or None
+    """
 
     item_mask_tensor = None
 
@@ -45,7 +57,13 @@ def list_to_tensor(item_list, min_len=0, max_len=0):
     return item_tensor, item_mask_tensor
 
 
-def pad_batch(batch, min_len=0, max_len=0, pad_value=0, left_padded=False):
+def pad_batch(
+    batch: List[Tensor],
+    min_len: int = 0,
+    max_len: int = 0,
+    pad_value: int = 0,
+    left_padded: bool = False,
+) -> Tuple[Tensor, Tensor]:
     """Convert the batch into a padded tensor and mask tensor.
 
     :param batch: The data for padding.
@@ -57,10 +75,10 @@ def pad_batch(batch, min_len=0, max_len=0, pad_value=0, left_padded=False):
     :param left_padding: if True, pad on the left, otherwise on the right.
     :type left_padding: boolean
     :return: The padded matrix and correspoing mask matrix.
-    :rtype: pair of torch.Tensors with shape (batch_size, max_seq_len)
+    :rtype: tuple of torch.Tensors with shape (batch_size, max_seq_len)
     """
     batch_size = len(batch)
-    max_seq_len = int(np.max([len(item) for item in batch]))
+    max_seq_len = int(np.max([item.size()[0] for item in batch]))
 
     if max_len > 0 and max_len < max_seq_len:
         max_seq_len = max_len
@@ -70,20 +88,18 @@ def pad_batch(batch, min_len=0, max_len=0, pad_value=0, left_padded=False):
     padded_batch = batch[0].new_full((batch_size, max_seq_len), pad_value)
 
     for i, item in enumerate(batch):
-        length = min(len(item), max_seq_len)
+        length = min(item.size()[0], max_seq_len)
         if left_padded:
             padded_batch[i, -length:] = item[-length:]
         else:
             padded_batch[i, :length] = item[:length]
 
-    mask_batch = torch.eq(padded_batch.clone().detach(), pad_value).type(
-        torch.BoolTensor
-    )
+    mask_batch = torch.eq(padded_batch.clone().detach(), pad_value)
 
     return padded_batch, mask_batch
 
 
-def prob_to_pred(probs):
+def prob_to_pred(probs: ndarray) -> ndarray:
     """Identify the class with the maximum probability (add 1 since we assume label
     class starts from 1)
 
@@ -94,7 +110,7 @@ def prob_to_pred(probs):
     return np.argmax(probs, axis=-1) + 1
 
 
-def pred_to_prob(preds, n_classes):
+def pred_to_prob(preds: ndarray, n_classes: int) -> ndarray:
     """Converts predicted labels to probabilistic labels
 
     :param preds: predicted labels
@@ -110,7 +126,7 @@ def pred_to_prob(preds, n_classes):
     return probs
 
 
-def move_to_device(obj, device=-1):
+def move_to_device(obj, device: Optional[int] = -1):  # type: ignore
     """
     Given a structure (possibly) containing Tensors on the CPU, move all the Tensors
     to the specified GPU (or do nothing, if they should beon the CPU).
@@ -123,7 +139,7 @@ def move_to_device(obj, device=-1):
     if device < 0 or not torch.cuda.is_available():
         return obj
     elif isinstance(obj, torch.Tensor):
-        return obj.cuda(device)
+        return obj.cuda(device)  # type: ignore
     elif isinstance(obj, dict):
         return {key: move_to_device(value, device) for key, value in obj.items()}
     elif isinstance(obj, list):
@@ -134,7 +150,9 @@ def move_to_device(obj, device=-1):
         return obj
 
 
-def array_to_numpy(array, flatten=False):
+def array_to_numpy(
+    array: Union[ndarray, List[Union[ndarray, int, float]]], flatten: bool = False
+) -> ndarray:
     """
     Covert an array to a numpy array.
 
@@ -161,7 +179,7 @@ def array_to_numpy(array, flatten=False):
     return array
 
 
-def merge(x, y):
+def merge(x: Dict[str, Any], y: Dict[str, Any]) -> Dict[str, Any]:
     """Merge two nested dictionaries. Overwrite values in x with values in y."""
 
     merged = {**x, **y}
@@ -175,7 +193,7 @@ def merge(x, y):
     return merged
 
 
-def str2bool(v):
+def str2bool(v: str) -> bool:
     if v.lower() in ("yes", "true", "t", "y", "1"):
         return True
     elif v.lower() in ("no", "false", "f", "n", "0"):
@@ -184,7 +202,7 @@ def str2bool(v):
         raise ValueError("Boolean value expected.")
 
 
-def str2dict(v):
+def str2dict(v: str) -> Dict[str, str]:
     dict = {}
     for token in v.split(","):
         key, value = token.split(":")
@@ -193,24 +211,26 @@ def str2dict(v):
     return dict
 
 
-def str2list(v, delim=","):
+def str2list(v: str, delim: str = ",") -> List[str]:
     return [t.strip() for t in v.split(delim)]
 
 
-def nullable_string(v):
+def nullable_string(v: str) -> Optional[str]:
     if not v or v.lower() in ["none", "null"]:
         return None
     return v
 
 
-def construct_identifier(task_name, data_name, split_name, metric_name=None):
+def construct_identifier(
+    task_name: str, data_name: str, split_name: str, metric_name: Optional[str] = None
+) -> str:
     if metric_name:
         return f"{task_name}/{data_name}/{split_name}/{metric_name}"
     else:
         return f"{task_name}/{data_name}/{split_name}"
 
 
-def random_string(length=5):
+def random_string(length: int = 5) -> str:
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase
     return "".join(random.choice(letters) for i in range(length))
