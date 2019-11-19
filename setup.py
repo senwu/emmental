14e82e3f45a088e3cdbcc494dd5162ccc6c1ee9a
Sen Wu <senwu@cs.stanford.edu>
2019-11-19 14:22:58 -0800

    fix torch version, sgd config, and use bool tensor

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "scikit-learn>=0.20.3, <0.21.0",
         "scipy>=1.1.0, <2.0.0",
         "tensorboardX>=1.6, <2.0",
-        "torch>=1.1.0, <2.0.0",
+        "torch>=1.3.1, <2.0.0",
         "tqdm>=4.36.0, <5.0.0",
     ],
     keywords=["emmental", "multi task learning", "deep learing"],
