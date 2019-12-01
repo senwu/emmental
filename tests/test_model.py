e9665036dec65709e2ffa64828f5ce286a8558d7
Sen Wu <senwu@cs.stanford.edu>
2019-12-01 03:01:29 -0800

    add model test

---
+++ tests/test_model.py
@@ -0,0 +1,100 @@
+import logging
+import shutil
+from functools import partial
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+import emmental
+from emmental import Meta
+from emmental.model import EmmentalModel
+from emmental.scorer import Scorer
+from emmental.task import EmmentalTask
+
+logger = logging.getLogger(__name__)
+
+
+def test_model(caplog):
+    """Unit test of model"""
+
+    caplog.set_level(logging.INFO)
+
+    dirpath = "temp_test_model"
+
+    Meta.reset()
+    emmental.init(dirpath)
+
+    def ce_loss(module_name, immediate_output_dict, Y, active):
+        return F.cross_entropy(
+            immediate_output_dict[module_name][0][active], (Y.view(-1))[active]
+        )
+
+    def output(module_name, immediate_output_dict):
+        return F.softmax(immediate_output_dict[module_name][0], dim=1)
+
+    task1 = EmmentalTask(
+        name="task_1",
+        module_pool=nn.ModuleDict(
+            {"m1": nn.Linear(10, 10, bias=False), "m2": nn.Linear(10, 2, bias=False)}
+        ),
+        task_flow=[
+            {"name": "m1", "module": "m1", "inputs": [("_input_", "data")]},
+            {"name": "m2", "module": "m2", "inputs": [("m1", 0)]},
+        ],
+        loss_func=partial(ce_loss, "m2"),
+        output_func=partial(output, "m2"),
+        scorer=Scorer(metrics=["accuracy"]),
+    )
+
+    model = EmmentalModel(name="test", tasks=task1)
+
+    assert model.name == "test"
+    assert model.task_names == set(["task_1"])
+    assert model.module_pool["m1"].module.weight.data.size() == (10, 10)
+    assert model.module_pool["m2"].module.weight.data.size() == (2, 10)
+
+    task1 = EmmentalTask(
+        name="task_1",
+        module_pool=nn.ModuleDict(
+            {"m1": nn.Linear(10, 5, bias=False), "m2": nn.Linear(5, 2, bias=False)}
+        ),
+        task_flow=[
+            {"name": "m1", "module": "m1", "inputs": [("_input_", "data")]},
+            {"name": "m2", "module": "m2", "inputs": [("m1", 0)]},
+        ],
+        loss_func=partial(ce_loss, "m2"),
+        output_func=partial(output, "m2"),
+        scorer=Scorer(metrics=["accuracy"]),
+    )
+
+    model.update_task(task1)
+
+    assert model.module_pool["m1"].module.weight.data.size() == (5, 10)
+    assert model.module_pool["m2"].module.weight.data.size() == (2, 5)
+
+    task2 = EmmentalTask(
+        name="task_2",
+        module_pool=nn.ModuleDict(
+            {"m1": nn.Linear(10, 5, bias=False), "m2": nn.Linear(5, 2, bias=False)}
+        ),
+        task_flow=[
+            {"name": "m1", "module": "m1", "inputs": [("_input_", "data")]},
+            {"name": "m2", "module": "m2", "inputs": [("m1", 0)]},
+        ],
+        loss_func=partial(ce_loss, "m2"),
+        output_func=partial(output, "m2"),
+        scorer=Scorer(metrics=["accuracy"]),
+    )
+
+    model.add_task(task2)
+
+    assert model.task_names == set(["task_1", "task_2"])
+
+    model.remove_task("task_1")
+    assert model.task_names == set(["task_2"])
+
+    model.save(f"{dirpath}/saved_model.pth")
+
+    model.load(f"{dirpath}/saved_model.pth")
+
+    shutil.rmtree(dirpath)
