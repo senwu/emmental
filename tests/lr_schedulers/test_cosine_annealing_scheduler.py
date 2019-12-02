69b77802e9d25abd3c67b50ee184c264bbeca0aa
Sen Wu <senwu@cs.stanford.edu>
2019-12-02 00:50:40 -0800

    add lr_scheduler tests

---
+++ tests/lr_schedulers/test_cosine_annealing_scheduler.py
@@ -0,0 +1,58 @@
+import logging
+import shutil
+
+import torch.nn as nn
+
+import emmental
+from emmental import Meta
+from emmental.learner import EmmentalLearner
+
+logger = logging.getLogger(__name__)
+
+
+def test_cosine_annealing_scheduler(caplog):
+    """Unit test of cosine annealing scheduler"""
+
+    caplog.set_level(logging.INFO)
+
+    lr_scheduler = "cosine_annealing"
+    dirpath = "temp_test_scheduler"
+    model = nn.Linear(1, 1)
+    emmental_learner = EmmentalLearner()
+
+    Meta.reset()
+    emmental.init(dirpath)
+
+    # Test default Adam setting
+    config = {
+        "learner_config": {
+            "n_epochs": 4,
+            "optimizer_config": {"optimizer": "sgd", "lr": 10},
+            "lr_scheduler_config": {"lr_scheduler": lr_scheduler},
+        }
+    }
+    emmental.Meta.update_config(config)
+    emmental_learner.n_batches_per_epoch = 1
+    emmental_learner._set_optimizer(model)
+    emmental_learner._set_lr_scheduler(model)
+
+    assert emmental_learner.optimizer.param_groups[0]["lr"] == 10
+
+    emmental_learner.lr_scheduler.step()
+    assert (
+        abs(emmental_learner.optimizer.param_groups[0]["lr"] - 8.535533905932738) < 1e-5
+    )
+
+    emmental_learner.lr_scheduler.step()
+    assert abs(emmental_learner.optimizer.param_groups[0]["lr"] - 5) < 1e-5
+
+    emmental_learner.lr_scheduler.step()
+    assert (
+        abs(emmental_learner.optimizer.param_groups[0]["lr"] - 1.4644660940672627)
+        < 1e-5
+    )
+
+    emmental_learner.lr_scheduler.step()
+    assert abs(emmental_learner.optimizer.param_groups[0]["lr"]) < 1e-5
+
+    shutil.rmtree(dirpath)
---
+++ tests/lr_schedulers/test_multi_step_scheduler.py
@@ -0,0 +1,60 @@
+import logging
+import shutil
+
+import torch.nn as nn
+
+import emmental
+from emmental import Meta
+from emmental.learner import EmmentalLearner
+
+logger = logging.getLogger(__name__)
+
+
+def test_multi_step_scheduler(caplog):
+    """Unit test of multi step scheduler"""
+
+    caplog.set_level(logging.INFO)
+
+    lr_scheduler = "multi_step"
+    dirpath = "temp_test_scheduler"
+    model = nn.Linear(1, 1)
+    emmental_learner = EmmentalLearner()
+
+    Meta.reset()
+    emmental.init(dirpath)
+
+    # Test default Adam setting
+    config = {
+        "learner_config": {
+            "n_epochs": 4,
+            "optimizer_config": {"optimizer": "sgd", "lr": 10},
+            "lr_scheduler_config": {
+                "lr_scheduler": lr_scheduler,
+                "multi_step_config": {
+                    "milestones": [1, 3],
+                    "gamma": 0.1,
+                    "last_epoch": -1,
+                },
+            },
+        }
+    }
+    emmental.Meta.update_config(config)
+    emmental_learner.n_batches_per_epoch = 1
+    emmental_learner._set_optimizer(model)
+    emmental_learner._set_lr_scheduler(model)
+
+    assert emmental_learner.optimizer.param_groups[0]["lr"] == 10
+
+    emmental_learner.lr_scheduler.step()
+    assert abs(emmental_learner.optimizer.param_groups[0]["lr"] - 1) < 1e-5
+
+    emmental_learner.lr_scheduler.step()
+    assert abs(emmental_learner.optimizer.param_groups[0]["lr"] - 1) < 1e-5
+
+    emmental_learner.lr_scheduler.step()
+    assert abs(emmental_learner.optimizer.param_groups[0]["lr"] - 0.1) < 1e-5
+
+    emmental_learner.lr_scheduler.step()
+    assert abs(emmental_learner.optimizer.param_groups[0]["lr"] - 0.1) < 1e-5
+
+    shutil.rmtree(dirpath)
