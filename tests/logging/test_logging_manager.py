397a3de73972b690a6a81966106fccd835fac299
Sen Wu <senwu@cs.stanford.edu>
2020-04-11 07:56:47 -0700

    logging manager test

--- tests/logging/test_logging_manager.py
+++ tests/logging/test_logging_manager.py
@@ -3,7 +3,9 @@ import logging
 import pytest
 
 import emmental
+from emmental.logging.log_writer import LogWriter
 from emmental.logging.logging_manager import LoggingManager
+from emmental.logging.tensorboard_writer import TensorBoardWriter
 from emmental.meta import Meta
 
 
@@ -178,8 +180,83 @@ def test_logging_manager_no_checkpointing(caplog):
     assert logging_manager.epoch_total == 2
 
 
+def test_logging_manager_json(caplog):
+    """Unit test of logging_manager (json)"""
+
+    caplog.set_level(logging.INFO)
+
+    emmental.init()
+    Meta.update_config(
+        config={
+            "logging_config": {
+                "counter_unit": "epoch",
+                "evaluation_freq": 1,
+                "checkpointing": False,
+                "checkpointer_config": {"checkpoint_freq": 2},
+                "writer_config": {"writer": "json"},
+            }
+        }
+    )
+
+    logging_manager = LoggingManager(n_batches_per_epoch=2)
+
+    logging_manager.update(5)
+
+    assert type(logging_manager.writer) == LogWriter
+
+
+def test_logging_manager_tensorboard(caplog):
+    """Unit test of logging_manager (tensorboard)"""
+
+    caplog.set_level(logging.INFO)
+
+    emmental.init()
+    Meta.update_config(
+        config={
+            "logging_config": {
+                "counter_unit": "epoch",
+                "evaluation_freq": 1,
+                "checkpointing": False,
+                "checkpointer_config": {"checkpoint_freq": 2},
+                "writer_config": {"writer": "tensorboard"},
+            }
+        }
+    )
+
+    logging_manager = LoggingManager(n_batches_per_epoch=2)
+
+    logging_manager.update(5)
+
+    assert type(logging_manager.writer) == TensorBoardWriter
+
+
+def test_logging_manager_no_writer(caplog):
+    """Unit test of logging_manager (no writer)"""
+
+    caplog.set_level(logging.INFO)
+
+    emmental.init()
+    Meta.update_config(
+        config={
+            "logging_config": {
+                "counter_unit": "epoch",
+                "evaluation_freq": 1,
+                "checkpointing": False,
+                "checkpointer_config": {"checkpoint_freq": 2},
+                "writer_config": {"writer": None},
+            }
+        }
+    )
+
+    logging_manager = LoggingManager(n_batches_per_epoch=2)
+
+    logging_manager.update(5)
+
+    assert logging_manager.writer is None
+
+
 def test_logging_manager_wrong_counter_unit(caplog):
-    """Unit test of logging_manager (no checkpointing)"""
+    """Unit test of logging_manager (wrong counter_unit)"""
 
     caplog.set_level(logging.INFO)
 
