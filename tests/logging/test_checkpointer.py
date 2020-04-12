ae3a57b5ffd22c31d95738c7d68e366aff05f623
Sen Wu <senwu@cs.stanford.edu>
2020-04-11 18:30:29 -0700

    add checkpointer tets

--- tests/logging/test_checkpointer.py
+++ tests/logging/test_checkpointer.py
@@ -60,7 +60,7 @@ def test_checkpointer_wrong_freq(caplog):
 
     with pytest.raises(ValueError):
         checkpointer = Checkpointer()
-        checkpointer.close()
+        checkpointer.clear()
 
 
 def test_checkpointer_wrong_metric_mode(caplog):
@@ -86,4 +86,90 @@ def test_checkpointer_wrong_metric_mode(caplog):
 
     with pytest.raises(ValueError):
         checkpointer = Checkpointer()
-        checkpointer.close()
+        checkpointer.clear()
+
+
+def test_checkpointer_clear_condition(caplog):
+    """Unit test of checkpointer (clear condition)"""
+
+    caplog.set_level(logging.INFO)
+
+    emmental.Meta.reset()
+
+    emmental.init()
+    emmental.Meta.update_config(
+        config={
+            "logging_config": {
+                "counter_unit": "sample",
+                "evaluation_freq": 10,
+                "checkpointing": True,
+                "checkpointer_config": {
+                    "clear_intermediate_checkpoints": True,
+                    "clear_all_checkpoints": True,
+                },
+            }
+        }
+    )
+
+    checkpointer = Checkpointer()
+    checkpointer.clear()
+
+    emmental.Meta.reset()
+
+    emmental.init()
+    emmental.Meta.update_config(
+        config={
+            "logging_config": {
+                "counter_unit": "sample",
+                "evaluation_freq": 10,
+                "checkpointing": True,
+                "checkpointer_config": {
+                    "clear_intermediate_checkpoints": False,
+                    "clear_all_checkpoints": True,
+                },
+            }
+        }
+    )
+
+    checkpointer = Checkpointer()
+    checkpointer.clear()
+
+    emmental.Meta.reset()
+
+    emmental.init()
+    emmental.Meta.update_config(
+        config={
+            "logging_config": {
+                "counter_unit": "sample",
+                "evaluation_freq": 10,
+                "checkpointing": True,
+                "checkpointer_config": {
+                    "clear_intermediate_checkpoints": True,
+                    "clear_all_checkpoints": False,
+                },
+            }
+        }
+    )
+
+    checkpointer = Checkpointer()
+    checkpointer.clear()
+
+    emmental.Meta.reset()
+
+    emmental.init()
+    emmental.Meta.update_config(
+        config={
+            "logging_config": {
+                "counter_unit": "sample",
+                "evaluation_freq": 10,
+                "checkpointing": True,
+                "checkpointer_config": {
+                    "clear_intermediate_checkpoints": False,
+                    "clear_all_checkpoints": False,
+                },
+            }
+        }
+    )
+
+    checkpointer = Checkpointer()
+    checkpointer.clear()
