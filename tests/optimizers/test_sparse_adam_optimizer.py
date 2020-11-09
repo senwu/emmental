dabbf89d411d28b5ee78a0e39ca1c34b2c8d1a2a
senwu <senwu@cs.stanford.edu>
2020-11-08 23:37:24 -0800

    fix sparse adam test

--- tests/optimizers/test_sparse_adam_optimizer.py
+++ tests/optimizers/test_sparse_adam_optimizer.py
@@ -11,6 +11,30 @@ from emmental.learner import EmmentalLearner
 logger = logging.getLogger(__name__)
 
 
+def grouped_parameters(model):
+    no_decay = ["bias"]
+    return [
+        {
+            "params": [
+                p
+                for n, p in model.named_parameters()
+                if not any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": emmental.Meta.config["learner_config"]["optimizer_config"][
+                "l2"
+            ],
+        },
+        {
+            "params": [
+                p
+                for n, p in model.named_parameters()
+                if any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.0,
+        },
+    ]
+
+
 def test_sparse_adam_optimizer(caplog):
     """Unit test of SparseAdam optimizer."""
     caplog.set_level(logging.INFO)
@@ -22,6 +46,10 @@ def test_sparse_adam_optimizer(caplog):
 
     Meta.reset()
     emmental.init(dirpath)
+
+    emmental.Meta.config["learner_config"]["optimizer_config"][
+        "parameters"
+    ] = grouped_parameters
 
     # Test default SparseAdam setting
     config = {"learner_config": {"optimizer_config": {"optimizer": optimizer}}}
