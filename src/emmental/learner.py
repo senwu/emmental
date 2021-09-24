2e267a1ccf94a071ffe4e3430f35d719dc19cabb
Sen Wu <senwu@cs.stanford.edu>
2021-09-23 17:17:43 -0700

    fix batch size count in learner when no y_dict

--- src/emmental/learner.py
+++ src/emmental/learner.py
@@ -688,7 +688,7 @@ class EmmentalLearner(object):
                 batch_size = 0
 
                 for uids, X_dict, Y_dict, task_to_label_dict, data_name, split in batch:
-                    batch_size += len(next(iter(Y_dict.values())))
+                    batch_size += len(uids)
 
                     # Perform forward pass and calcualte the loss and count
                     uid_dict, loss_dict, prob_dict, gold_dict = model(
