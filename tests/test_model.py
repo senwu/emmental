d70279049f24a4ddab27770bd828cc6d20d24013
Sen Wu <senwu@cs.stanford.edu>
2021-10-06 02:34:27 -0700

    more tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,10 +3,12 @@ import logging
 import shutil
 from functools import partial
 
+import pytest
 from torch import nn as nn
 from torch.nn import functional as F
 
 from emmental import EmmentalModel, EmmentalTask, Meta, Scorer, init
+from emmental.modules.identity_module import IdentityModule
 
 logger = logging.getLogger(__name__)
 
@@ -109,3 +111,82 @@ def test_model(caplog):
     assert model.task_names == set(["task_1", "task_2"])
 
     shutil.rmtree(dirpath)
+
+
+def test_model_invalid_task(caplog):
+    """Unit test of model with invalid task."""
+    caplog.set_level(logging.INFO)
+
+    dirpath = "temp_test_model_with_invalid_task"
+
+    Meta.reset()
+    init(dirpath)
+
+    task_name = "task1"
+
+    task = EmmentalTask(
+        name=task_name,
+        module_pool=nn.ModuleDict(
+            {
+                "input_module0": IdentityModule(),
+                f"{task_name}_pred_head": IdentityModule(),
+            }
+        ),
+        task_flow=[
+            {
+                "name": "input1",
+                "module": "input_module0",
+                "inputs": [("_input_", "data")],
+            },
+            {
+                "name": f"{task_name}_pred_head",
+                "module": f"{task_name}_pred_head",
+                "inputs": [("input1", 0)],
+            },
+        ],
+        module_device={"input_module0": -1},
+        loss_func=None,
+        output_func=None,
+        action_outputs=None,
+        scorer=None,
+        require_prob_for_eval=False,
+        require_pred_for_eval=True,
+    )
+
+    task1 = EmmentalTask(
+        name=task_name,
+        module_pool=nn.ModuleDict(
+            {
+                "input_module0": IdentityModule(),
+                f"{task_name}_pred_head": IdentityModule(),
+            }
+        ),
+        task_flow=[
+            {
+                "name": "input1",
+                "module": "input_module0",
+                "inputs": [("_input_", "data")],
+            },
+            {
+                "name": f"{task_name}_pred_head",
+                "module": f"{task_name}_pred_head",
+                "inputs": [("input1", 0)],
+            },
+        ],
+        module_device={"input_module0": -1},
+        loss_func=None,
+        output_func=None,
+        action_outputs=None,
+        scorer=None,
+        require_prob_for_eval=False,
+        require_pred_for_eval=True,
+    )
+
+    model = EmmentalModel(name="test")
+    model.add_task(task)
+
+    with pytest.raises(ValueError):
+        model.add_task(task1)
+
+    with pytest.raises(ValueError):
+        model.add_task(task_name)
